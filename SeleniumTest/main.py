91f81a8fbcb1f0b8c7b139b8820074af56ed6853
VitalyBorzunov <303919@mail.ru>
2023-10-15 21:57:01 +0300

    2 commit

--- SeleniumTest/main.py
+++ SeleniumTest/main.py
@@ -5,53 +5,54 @@ token = "4820d3fa9b872c438462ac46114fd094"
 host = "https://api.pokemonbattle.me:9104"
 
 
-'''response = requests.post('https://api.pokemonbattle.me:9104/trainers/reg',
-                         json={"trainer_token": token,
-                               "email": "ns9pi@rentforsale7.com",
-                               "password": "Iloveqa1"},
-                                 headers={"Content-Type" : "application/json"})
-print(response.text)'''
-
-'''
-response_activation = requests.post(f'{host}/trainers/confirm_email',
-                                    json={"trainer_token" : token},
-                                    headers={"Content-Type" : "application/json"})
-print(response_activation.status_code)
-
-if response_activation.status_code == 200:
-    print('ok!')
-else:
-    print('not ok!')'''
+#response = requests.post('https://api.pokemonbattle.me:9104/trainers/reg',
+#                         json={"trainer_token": token,
+#                               "email": "ns9pi@rentforsale7.com",
+#                               "password": "Iloveqa1"},
+#                                 headers={"Content-Type" : "application/json"})
+#print(response.text)
 
 
+#response_activation = requests.post(f'{host}/trainers/confirm_email',
+#                                    json={"trainer_token" : token},
+#                                    headers={"Content-Type" : "application/json"})
+#print(response_activation.status_code)
+#
+#if response_activation.status_code == 200:
+#    print('ok!')
+#else:
+#    print('not ok!')
 
 
-''' создание пакемена'''
+
+#создание пакемена
 response_create_pokemon = requests.post(f'{host}/pokemons',
                                    json={"name": "АвтотестерСан", 
                                          "photo": "https://dolnikov.ru/pokemons/albums/001.png"},
                                    headers={"trainer_token":token, "Content-Type" : "application/json"})
-print(response_create_pokemon.text)
+id_pok =response_create_pokemon.json()["id"]
+print(response_create_pokemon.json()["id"])
 
-'''смена имени покемона'''
+
+#смена имени покемона
 response_change_name = requests.put(f'{host}/pokemons',
-                                    json={"pokemon_id": 12524,
-                                    "name": "KANZAS",
+                                    json={"pokemon_id": id_pok,
+                                    "name": "KANZAS_VET",
                                     "photo": "https://dolnikov.ru/pokemons/albums/001.png"},
                                     headers={"trainer_token": token, "Content-Type" : "application/json"})
 print(response_change_name.text)
 
 
-'''поймать пакемона в пакетбол'''
+#поймать пакемона в пакетбол
 response_catch = requests.post(f'{host}/trainers/add_pokeball',
-                               json={"pokemon_id": 12524},
+                               json={"pokemon_id": id_pok},
                                headers={"trainer_token": token, "Content-Type" : "application/json"})
 print(response_catch.text)
 
 
-''' убить покемона'''
+ #убить покемона
 response_kill_pokemon = requests.post(f'{host}/pokemons/kill',
-                                      json={ "pokemon_id": "12522"},
+                                      json={ "pokemon_id": id_pok},
                                       headers={"trainer_token": token, "Content-Type" : "application/json"})
 print(response_kill_pokemon.text)
 
